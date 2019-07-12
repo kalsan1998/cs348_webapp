75c3a185c268bfa8c8182cdbcc09e57a4182f591
Edwin Joe <ejoe@edu.uwaterloo.ca>
2019-07-12 19:19:24 -0400

    Prepopulate supplier table with one entry

--- setup/data.sql
+++ setup/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO venue(venue_name, max_capacity, venue_address, cost_per_hour) VALUES(
     'UW', 100, '123 University Ave.', 100.35);
+
+INSERT INTO supplier(company_name, company_phone, company_email, company_address) VALUES(
+    'UW Food Service', '1234567890', 'food@uw.com', '777 University Avenue'
+);
