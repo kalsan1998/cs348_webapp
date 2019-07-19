be8fdc9fdb7aa3e27bff1c9d596c6d956d4b85b5
Kaan Alsan <kaan.alsan@live.com>
2019-07-19 12:05:47 -0400

    Update varchar lengths in schema

--- setup/schema.sql
+++ setup/schema.sql
@@ -3,23 +3,23 @@ CREATE TABLE venue(
   venue_name VARCHAR(64) NOT NULL,
   description VARCHAR(256),
   max_capacity INTEGER NOT NULL CHECK(max_capacity >= 0),
-  venue_address VARCHAR(24) UNIQUE NOT NULL,
+  venue_address VARCHAR(256) UNIQUE NOT NULL,
   cost_per_hour DECIMAL(6,2) NOT NULL,
   PRIMARY KEY (venue_id)
 );
 
 CREATE TABLE supplier(
   supplier_id SERIAL,
-  company_name VARCHAR(24) NOT NULL,
+  company_name VARCHAR(256) NOT NULL,
   company_phone CHAR(10) NOT NULL,
-  company_email VARCHAR(24),
-  company_address VARCHAR(24),
+  company_email VARCHAR(256),
+  company_address VARCHAR(256),
   PRIMARY KEY (supplier_id)
 );
 
 CREATE TABLE supply(
   supplier_id SERIAL,
-  supply_name VARCHAR(24),
+  supply_name VARCHAR(256),
   description VARCHAR(256),
   price_per_quantity DECIMAL(6,2) NOT NULL,
   min_quantity INTEGER CHECK(min_quantity >= 0),
@@ -30,15 +30,15 @@ CREATE TABLE supply(
  
 CREATE TABLE decoration(
   supplier_id SERIAL,
-  supply_name VARCHAR(24),
-  type VARCHAR(24),
+  supply_name VARCHAR(256),
+  type VARCHAR(256),
   PRIMARY KEY (supplier_id, supply_name),
   FOREIGN KEY (supplier_id, supply_name) REFERENCES supply (supplier_id, supply_name) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE entertainment(
   supplier_id SERIAL,
-  supply_name VARCHAR(24),
+  supply_name VARCHAR(256),
   duration TIME NOT NULL,
   PRIMARY KEY (supplier_id, supply_name),
   FOREIGN KEY (supplier_id, supply_name) REFERENCES supply (supplier_id, supply_name) ON DELETE CASCADE ON UPDATE CASCADE
@@ -46,7 +46,7 @@ CREATE TABLE entertainment(
 
 CREATE TABLE food_item(
   supplier_id SERIAL,
-  supply_name VARCHAR(24),
+  supply_name VARCHAR(256),
   is_vegetarian BOOLEAN,
   is_vegan BOOLEAN,
   is_gluten_free BOOLEAN,
@@ -57,15 +57,15 @@ CREATE TABLE food_item(
 );
 
 CREATE TABLE app_user(
-  email VARCHAR(24),
-  password VARCHAR(24) NOT NULL,
+  email VARCHAR(256),
+  password VARCHAR(256) NOT NULL,
   first_name VARCHAR(12) NOT NULL,
   last_name VARCHAR(12) NOT NULL,
   PRIMARY KEY (email)
 );
 
 CREATE TABLE employee(
-  email VARCHAR(24),
+  email VARCHAR(256),
   sin CHAR(10),
   salary DECIMAL(8,2),
   PRIMARY KEY(email),
@@ -73,8 +73,8 @@ CREATE TABLE employee(
 );
 
 CREATE TABLE client(
-  email VARCHAR(24),
-  home_address VARCHAR(24),
+  email VARCHAR(256),
+  home_address VARCHAR(256),
   phone_number CHAR(10),
   PRIMARY KEY(email),
   FOREIGN KEY (email) REFERENCES app_user (email) ON DELETE CASCADE ON UPDATE CASCADE
@@ -83,11 +83,10 @@ CREATE TABLE client(
 CREATE TABLE billing_information(
   billing_id SERIAL,
   card_number CHAR(16) NOT NULL,
-  expiration_date CHAR(4) NOT NULL,
-  cardholder_name VARCHAR(30) NOT NULL,
-  cvv CHAR(3) NOT NULL,
-  billing_address VARCHAR(24) NOT NULL,
-  client_email VARCHAR(24) NOT NULL,
+  expiration_date CHAR(4) NOT NULL,cardholder_name VARCHAR(30) NOT NULL,
+  cardholder_name VARCHAR(64) NOT NULL,
+  billing_address VARCHAR(256) NOT NULL,
+  client_email VARCHAR(256) NOT NULL,
   date_added DATE NOT NULL,
   PRIMARY KEY (billing_id),
   FOREIGN KEY (client_email) REFERENCES client (email) ON DELETE CASCADE ON UPDATE CASCADE
@@ -99,7 +98,7 @@ CREATE TABLE event(
   event_datetime TIMESTAMP,
   event_duration TIME NOT NULL,
   attendees INTEGER NOT NULL,
-  manager VARCHAR(24) NOT NULL,
+  manager VARCHAR(256) NOT NULL,
   total_cost DECIMAL(7,2) NOT NULL,
   booking_date DATE NOT NULL,
   PRIMARY KEY (billed_to, venue_id, event_datetime),
@@ -113,7 +112,7 @@ CREATE TABLE supply_order(
   venue_id SERIAL,
   event_datetime TIMESTAMP,
   supplier_id SERIAL,
-  supply_name VARCHAR(24),
+  supply_name VARCHAR(256),
   supply_quantity INTEGER NOT NULL,
   supply_cost DECIMAL(8,2) NOT NULL,
   PRIMARY KEY (billed_to, venue_id, event_datetime, supplier_id, supply_name),
